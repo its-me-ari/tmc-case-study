aab159234cfb7714783ccc25151a5bead3c94e5e
Ari Rahmat Firdaus <ari.rahmat@bankmandiri.co.id>
2022-09-07 00:25:21 +0700

    using controller advice to handle exception

--- src/main/java/com/example/tmccasestudy/adapters/input/rest/CategoryController.java
+++ src/main/java/com/example/tmccasestudy/adapters/input/rest/CategoryController.java
@@ -3,15 +3,10 @@ package com.example.tmccasestudy.adapters.input.rest;
 import com.example.tmccasestudy.adapters.input.rest.data.ApiResponse;
 import com.example.tmccasestudy.ports.input.CreateCategoryService;
 import com.example.tmccasestudy.domain.Category;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api/categories")
@@ -31,16 +26,4 @@ public class CategoryController {
         return ResponseEntity.ok(categoryResponse);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
 }
--- src/main/java/com/example/tmccasestudy/adapters/input/rest/ProductController.java
+++ src/main/java/com/example/tmccasestudy/adapters/input/rest/ProductController.java
@@ -6,13 +6,9 @@ import com.example.tmccasestudy.domain.Product;
 import com.example.tmccasestudy.ports.input.CreateProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api/products")
@@ -30,19 +26,6 @@ public class ProductController {
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setData(newProduct);
         return ResponseEntity.ok(apiResponse);
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 
 }
