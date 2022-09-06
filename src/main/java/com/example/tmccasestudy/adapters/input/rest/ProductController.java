3ecbc2d06e19b68db8b01355fa981bc6efd753cf
Ari Rahmat Firdaus <ari.rahmat@bankmandiri.co.id>
2022-09-07 00:08:29 +0700

    add product validation

--- src/main/java/com/example/tmccasestudy/adapters/input/rest/ProductController.java
+++ src/main/java/com/example/tmccasestudy/adapters/input/rest/ProductController.java
@@ -4,10 +4,15 @@ import com.example.tmccasestudy.adapters.input.rest.data.ApiResponse;
 import com.example.tmccasestudy.adapters.input.rest.data.ProductRequest;
 import com.example.tmccasestudy.domain.Product;
 import com.example.tmccasestudy.ports.input.CreateProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/products")
@@ -25,6 +30,19 @@ public class ProductController {
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setData(newProduct);
         return ResponseEntity.ok(apiResponse);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 
 }
