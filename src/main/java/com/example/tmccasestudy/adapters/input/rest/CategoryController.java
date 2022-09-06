35b87af8535c6b6e217eeae7b4bd7b878706aa20
Ari Rahmat Firdaus <ari.rahmat@bankmandiri.co.id>
2022-09-06 22:31:15 +0700

    add name validation

--- src/main/java/com/example/tmccasestudy/adapters/input/rest/CategoryController.java
+++ src/main/java/com/example/tmccasestudy/adapters/input/rest/CategoryController.java
@@ -3,11 +3,15 @@ package com.example.tmccasestudy.adapters.input.rest;
 import com.example.tmccasestudy.adapters.input.rest.data.CategoryResponse;
 import com.example.tmccasestudy.ports.input.CreateCategoryService;
 import com.example.tmccasestudy.domain.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/categories")
@@ -20,10 +24,23 @@ public class CategoryController {
     }
 
     @PostMapping
-    public ResponseEntity<?> submitCategory(@RequestBody Category category) {
+    public ResponseEntity<?> submitCategory(@RequestBody @Valid Category category) {
         Category newCategory = service.save(category);
         CategoryResponse categoryResponse = new CategoryResponse();
         categoryResponse.setData(newCategory);
         return ResponseEntity.ok(categoryResponse);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
