362b8cac74e20c62adac24b15ea1c860e6c8e376
Ari Rahmat Firdaus <ari.rahmat@bankmandiri.co.id>
2022-09-15 13:12:40 +0700

    unit test: create category API

---
+++ src/test/java/com/example/tmc/cqrs/service/CategoryCommandServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.tmc.cqrs.service;
+
+import com.example.tmc.cqrs.entity.Category;
+import com.example.tmc.cqrs.repository.CategoryRepository;
+import com.example.tmc.cqrs.service.impl.CategoryCommandServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryCommandServiceTest {
+
+    private final String CATEGORY_NAME = "Category Name";
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryCommandServiceImpl categoryCommandService;
+
+    private Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = Category.builder()
+                .name(CATEGORY_NAME) // only supply name variable
+                .build();
+    }
+
+    @Test
+    void createCategory() {
+
+        // given
+        given(categoryRepository.save(category)).willReturn(category);
+
+        // when
+        categoryCommandService.createCategory(category);
+
+        // then
+        assertThat(category.getId()).isNotBlank();
+        assertThat(category.getName()).isNotBlank().isEqualTo(CATEGORY_NAME);
+        assertThat(category.getCreatedAt()).isGreaterThan(0);
+    }
+}
